6b233676631e5d46eb88e37468978ab5c9a62b6c
Wangsta <alexrwang@gmail.com>
2015-04-09 10:06:00 -0500

    Updated Backend and Encoder

--- src/Backend.java
+++ src/Backend.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 
 public class Backend {
+	
+		    public static Map<String, List<String>> myData = new TreeMap<String, List<String>>();
+		    
+		    public static Encoder hi = new Encoder("messages");
+		    
+		  public static List<String> getMessage(String username)
+		  {
+		    return myData.get(username);
+		  }
 
+		  public static void addMessage(String username, String msg){
+		    if(myData.containsKey(username)){
+		    	hi.write(msg);
+		    	myData.get(username).add(msg);
+		    }
+		    else{
+		    	myData.put(username, new ArrayList<String>());
+		    }
+		  }
+
+		  public static List<String> getUsers(){
+			List<String> ff = new ArrayList<String>();
+			ff.addAll(myData.keySet());
+		    return ff;
+		  
+
+		}
 }
--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,4 +1,33 @@
+import java.awt.Dimension;
 
-public class FrontEnd {
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
+
+public class FrontEnd 
+{
+	public FrontEnd()
+	{
+		JFrame myFrame = new JFrame("Messages");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setMinimumSize(new Dimension(900,650));
+		myFrame.setLocation(300,120);
+		
+		myFrame.pack();
+		myFrame.setVisible(true);
+	}
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				new FrontEnd();
+			}
+		});
+	}
 }
